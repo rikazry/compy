7a53e79a1369c12eaf96e89925c33b0ea4e51839
rikazry <rika.zry@gmail.com>
2014-09-01 01:30:25 -0400

    added .gitignore and neville algo for poly interpolation in fitting.py

--- fitting.py
+++ fitting.py
@@ -1,9 +1,9 @@
 from sympy.polys.specialpolys import interpolating_poly
 from sympy.abc import x
-from sympy.core import Add, Mul
 
-def inter_poly(X, Y, symbol = x, algo = 'Lr', expand = True):
+def inter_poly(X, Y, symbol = x, algo = 'Lr', simplify = True):
     """
+    polynomial interpolation using Lagrange's Methods
     takes numpy array
     """
     n = X.size
@@ -11,12 +11,15 @@ def inter_poly(X, Y, symbol = x, algo = 'Lr', expand = True):
         poly = interpolating_poly(n, symbol, X, Y)
     elif algo == 'Nt':
         poly = interpolating_poly_Newton(n, symbol, X, Y)
-    poly = poly.expand() if expand == True else 0
+    elif algo == 'Nv':
+        poly = interpolating_poly_Neville(n, symbol, X, Y)
+    if simplify == True:
+        poly = poly.expand() 
     return poly
 
 def interpolating_poly_Newton(n, symbol, X,Y):
     """
-    polynomial interpolation using Newton's Methos
+    polynomial interpolation using Newton's Methods
     """
     def coef(X, Y):
         """
@@ -33,3 +36,12 @@ def interpolating_poly_Newton(n, symbol, X,Y):
         poly = a[n-i-1] + (symbol - X[n-i-1])*poly
     return poly
 
+def interpolating_poly_Neville(n, symbol, X, Y):
+    """
+    polynomial interpolation using Neville's Methods
+    currently only support numerical result, not symbolic result
+    """
+    poly = Y.copy()
+    for i in range(1, n):
+        poly[0:n-i] = ((symbol - X[i:n])*poly[0:n-i]+(X[0:n-i] - symbol)*poly[1:n-i+1])/(X[0:n-i] - X[i:n])
+    return poly[0]
